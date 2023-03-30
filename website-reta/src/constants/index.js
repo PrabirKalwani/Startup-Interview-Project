3131e1a1c85e9a54c296d2a31b434201c69e808f
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-03-31 03:27:46 +0530

    Added Retaverse Website
    BT Only with burshe

---
+++ website-reta/src/constants/index.js
@@ -0,0 +1,128 @@
+import { people01, people02, people03, facebook, instagram, linkedin, twitter, airbnb, binance, coinbase, dropbox, send, shield, star  } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "About Us",
+  },
+  {
+    id: "features",
+    title: "Remind Me",
+  },
+  
+];
+
+export const features = [
+  {
+    id: "feature-1",
+    icon: star,
+    title: "customer acquisition costs",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "lifetime value",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "retention metrics",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "feature-1",
+    icon: star,
+    title: "aggregator platform managements",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "feature-2",
+    icon: shield,
+    title: "sentiment analysis",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: "feature-3",
+    icon: send,
+    title: "online reputation management",
+    content:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
+
+export const feedback = [
+  {
+    id: "feedback-1",
+    content:
+      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
+    name: "Herman Jensen",
+    title: "Founder & Leader",
+    img: people01,
+  },
+  {
+    id: "feedback-2",
+    content:
+      "Money makes your life easier. If you're lucky to have it, you're lucky.",
+    name: "Steve Mark",
+    title: "Founder & Leader",
+    img: people02,
+  },
+  {
+    id: "feedback-3",
+    content:
+      "It is usually people in the money business, finance, and international trade that are really rich.",
+    name: "Kenn Gallagher",
+    title: "Founder & Leader",
+    img: people03,
+  },
+];
+
+export const stats = [
+  {
+    id: "stats-1",
+    title: "awsomestat",
+    value: "69420",
+  },
+  {
+    id: "stats-2",
+    title: "moreawsomestat",
+    value: "69420",
+  },
+  {
+    id: "stats-3",
+    title: "superstat",
+    value: "69420",
+  },
+];
+
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
