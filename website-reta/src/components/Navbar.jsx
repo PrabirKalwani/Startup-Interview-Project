79c6c2a6027d08fc6801016cc04c181132c6d21a
PrabirKalwani <70889682+PrabirKalwani@users.noreply.github.com>
2023-06-23 22:37:02 +0530

    Changes-website

--- website-reta/src/components/Navbar.jsx
+++ website-reta/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="retaverse" className="w-[10px] h-[30px] " />
+      <img src={logo} alt="Logo" className="w-[10px] h-[30px] " />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
